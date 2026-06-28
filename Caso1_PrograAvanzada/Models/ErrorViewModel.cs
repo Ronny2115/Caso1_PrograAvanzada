1c6bd8c834a2dd5693376a21647f7446fe6c29d7
Ronny2115 <andres05bonilla@gmail.com>
2026-06-27 21:30:10 -0600

    Initial commit

---
+++ Caso1_PrograAvanzada/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Caso1_PrograAvanzada.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
